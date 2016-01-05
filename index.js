77fdc463f4cc314a2eea403f5108bc3833383be0
yanbingbing <me@yanbingbing.com>
2016-01-05 14:43:32 +0800

    easy

--- index.js
+++ index.js
@@ -2,15 +2,7 @@ var map = require('map-stream'),
 	gutil = require('gulp-util'),
 	PluginError = gutil.PluginError;
 
-module.exports = function(options){
-	if(!options.name){
-		throw new PluginError('gulp-wrap-factory', 'Variable not specified');
-	}
-
-	if(/^[^A-z0-9$_]+$/.test(options.name)){
-		throw new PluginError('gulp-wrap-factory', 'Incorrect global variable name');
-	}
-
+module.exports = function(){
 	return map(function(file, cb){
 		if (file.isNull()) {
 			return cb(null, file);
@@ -20,21 +12,18 @@ module.exports = function(options){
 			return cb(new PluginError('gulp-wrap-factory', 'Streaming not supported'));
 		}
 
-		file.contents = new Buffer(wrapper(file.contents.toString(), options.name).join('\n'));
+		file.contents = new Buffer(wrapper(file.contents.toString()).join('\n'));
 
 		cb(null, file);
 	});
 };
 
-function wrapper(code, name){
+function wrapper(code) {
   return [
-    '(function (global, factory) {',
-      'if (typeof exports !== "undefined") {',
-         'module.exports = factory;',
-      '} else {',
-        'global.'+ name +' = factory;',
-      '}',
-    '})(this, function (window) {var document=window.document;',
-      'return (function () {return '+code+'}).call(window)',
-    '});'];
+    'module.exports = function (window) {',
+    '  var document = window.document;',
+    '  return (function () {',
+    '    return ' + code,
+    '  }).call(window);',
+    '};'];
 }
